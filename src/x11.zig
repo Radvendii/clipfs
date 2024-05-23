6dcbcd3cf67a1755833adc540281f51b0a29d44e
Taeer Bar-Yam <taeer.bar-yam@moduscreate.com>
2024-05-22 23:59:19 -0400

    comptime checkErrors

--- src/x11.zig
+++ src/x11.zig
@@ -1,4 +1,57 @@
+const std = @import("std");
 pub const c = @import("c.zig");
+
+// x11 has a bunch of error codes e.g. c.BadAlloc, c.BadValue, etc.
+// this converts them all from c.Foo to error.Foo for a given set of error values possible
+// this could be cleaner with e.g. https://github.com/ziglang/zig/issues/12250
+// we could cast it to an enum with all the errors defined, and then in then inline else the conversion
+// checkErrors(error{Foo, Bar}, val);
+// =>
+// switch (val) {
+//      c.Foo => error.Foo,
+//      c.Bar => error.Bar,
+//      else => val
+//  }
+// I'm still not decided which syntax I like better. checkErrors() is kind of messy and less readable, but also less error prone and repetitive
+// I suspect this is a problem I will keep coming back to with comptime
+fn checkErrors(val: anytype, comptime errorset: type) errorset!@TypeOf(val) {
+    switch (@typeInfo(errorset)) {
+        .ErrorSet => |_errs| {
+            const errs = _errs orelse
+                @compileError("got null errorset. not sure yet what that means\n");
+            inline for (errs) |err| {
+                if (!@hasDecl(c, err.name)) {
+                    @compileError("x11 does not define the error code " ++ err.name);
+                }
+                if (val == @field(c, err.name)) {
+                    return @field(anyerror, err.name);
+                }
+            }
+            return val;
+        },
+        else => @compileError("checkErrors must be called with an errorset\n"),
+    }
+}
+
+test {
+    const err = checkErrors(c.BadAlloc, error{BadAlloc});
+    if (@TypeOf(err) != @TypeOf(error.BadAlloc)) {
+        return error.NotError;
+    }
+    if (err != error.BadAlloc) {
+        return error.NotBadAlloc;
+    }
+}
+
+test {
+    const err = checkErrors(c.BadValue, error{BadAlloc});
+    if (@TypeOf(err) == @TypeOf(error.BadAlloc)) {
+        return error.MistakenError;
+    }
+    if (err != c.BadValue) {
+        return error.FailedRoundTrip;
+    }
+}
 
 pub const Atom = struct {
     raw: c.Atom,
@@ -13,8 +66,10 @@ pub const Display = struct {
         return .{ .raw = raw };
     }
     pub fn close(dpy: Display) !void {
-        if (c.XCloseDisplay(dpy.raw) == c.BadGC)
-            return error.BadGC;
+        _ = try checkErrors(
+            c.XCloseDisplay(dpy.raw),
+            error{BadGC},
+        );
     }
 
     pub fn defaultScreen(dpy: Display) Screen {
@@ -24,13 +79,12 @@ pub const Display = struct {
         };
     }
     pub fn internAtom(dpy: Display, atom_name: [:0]const u8, only_if_exists: bool) !Atom {
-        switch (c.XInternAtom(dpy.raw, atom_name.ptr, @intCast(@intFromBool(only_if_exists)))) {
-            c.BadAlloc => return error.BadAlloc,
-            c.BadValue => return error.BadValue,
-            // XXX: do we want this to be an error, or a valid return?
-            c.None => return error.None,
-            else => |atom| return .{ .raw = atom },
-        }
+        const maybe_atom = c.XInternAtom(dpy.raw, atom_name.ptr, @intCast(@intFromBool(only_if_exists)));
+        const atom = try checkErrors(
+            maybe_atom,
+            error{ BadAlloc, BadValue, None },
+        );
+        return .{ .raw = atom };
     }
 };
 
@@ -65,7 +119,7 @@ pub const Window = struct {
         border: u64,
         background: u64,
     ) !Window {
-        const raw = c.XCreateSimpleWindow(
+        const maybe_win = c.XCreateSimpleWindow(
             parent.dpy.raw,
             parent.raw,
             @bitCast(x),
@@ -76,37 +130,34 @@ pub const Window = struct {
             @bitCast(border),
             @bitCast(background),
         );
-        switch (raw) {
-            c.BadAlloc => return error.BadAlloc,
-            c.BadMatch => return error.BadMatch,
-            c.BadValue => return error.BadValue,
-            c.BadWindow => return error.BadWindow,
-            else => {},
-        }
+        const win = try checkErrors(
+            maybe_win,
+            error{ BadAlloc, BadMatch, BadValue, BadWindow },
+        );
         return .{
             .dpy = parent.dpy,
-            .raw = raw,
+            .raw = win,
         };
     }
 
     pub fn map(win: Window) !void {
-        switch (c.XMapWindow(win.dpy.raw, win.raw)) {
-            c.BadWindow => return error.BadWindow,
-            else => {},
-        }
+        _ = try checkErrors(
+            c.XMapWindow(win.dpy.raw, win.raw),
+            error{BadWindow},
+        );
     }
 
     pub fn mapRaised(win: Window) !void {
-        switch (c.XMapRaised(win.dpy.raw, win.raw)) {
-            c.BadWindow => return error.BadWindow,
-            else => {},
-        }
+        _ = try checkErrors(
+            c.XMapRaised(win.dpy.raw, win.raw),
+            error{BadWindow},
+        );
     }
 
     pub fn mapSubwindows(win: Window) !void {
-        switch (c.XMapSubwindows(win.dpy.raw, win.raw)) {
-            c.BadWindow => return error.BadWindow,
-            else => {},
-        }
+        _ = try checkErrors(
+            c.XMapSubwindows(win.dpy.raw, win.raw),
+            error{BadWindow},
+        );
     }
 };
